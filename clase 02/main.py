d0faaa8fc0941eb750db0bbdba6d740d477b7562
ingridvanesa01 <ingridcotacovanesa@gmail.com>
2026-05-16 12:23:38 -0600

    comentar la clase para mejorar entendimiento

---
+++ clase 02/main.py
@@ -0,0 +1,30 @@
+ inici#alizar variables
+nombre:str = "ingrid"
+
+#solicitar al usuario su edad
+edad:int = int (input cuantos años tienes)
+
+#calcular año de nacimiento
+año_de_nacimiento:int = 2026 - edad 
+
+#imprimir nombre edad y año de nacimiento
+print(nombre)
+print ("mi edad es:" , edad)
+print("mi año de nacimiento", año_de_naciento)
+
+# calcular mayoria de edad
+if edad >= 19:
+ print("soy mayor de edad")
+else:
+ print("soy menor de edad")
+ 
+# inicializar variables
+soy_yo = (nombre == "ingrid" and edad == 19)
+no_soy_yo = not (nombre == "ingrid" and edad == 19)
+talvez_soy_yo = nombre == "ingrid" or edad == 19
+
+#incremento
+x:int = 10
+x += 5
+print (x)
+
